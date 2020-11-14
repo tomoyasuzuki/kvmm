7bc74695f80edf90566f957cd77fca238039cf78
tomoyasuzuki <tomoya.s0121@icloud.com>
2020-11-14 14:40:09 +0900

    small guest binary works

--- guest.s
+++ guest.s
@@ -1,10 +1,8 @@
-.file   "guest.s"
-.data
-.bss
 .text
 .globl _start
+.code16
 _start:
-        movw %ax, 0x400
-        out %ax, $0x10
-        hlt
-        ret
+    xorw %ax, %ax
+loop:
+    hlt
+    jmp loop
